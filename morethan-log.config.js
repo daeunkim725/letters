dbb54cff6c8cd26aed998bee54eeeda0b2b3fbb1
daeunkim725 <118401980+daeunkim725@users.noreply.github.com>
2023-01-01 14:57:40 -0500

    Update morethan-log.config.js

--- morethan-log.config.js
+++ morethan-log.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting
   profile: {
     name: 'Daeun Kim',
-    image: '/avatar.svg',  // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: '/avatar.png',  // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: 'me',
     bio: 'Hello! I am a student at Princeton University studying Operations Research and Financial Engineering. I love cats, music, books, and all things math',
     email: 'dk4994@princeton.edu',
